c18f6e99375122e8f221623820e9a80f08c2dc16
Chris Chang Change <changc21@gmatc.matc.edu>
2023-11-25 22:05:54 -0600

    Created topbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import React from "react";
-import { CssBaseline, Grid } from '@material-ui/core';
+import Home from "./pages/home/Home";
 
-import Header from "./components/Header/Header";
-import List from "./components/List/List";
-import Map from "./components/Map/Map";
-
-const App = () => {
-    return (
-        <>
-            <CssBaseline />
-            <Header />
-            <Grid constainer spacing={3} style={{width: '100%'}}>
-                <Grid item xs={12} md={4}>
-                    <List />
-                </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map />
-                </Grid>
-            </Grid>
-        </>
-    )
+function App() {
+  return <Home/>
 }
 
 export default App;
